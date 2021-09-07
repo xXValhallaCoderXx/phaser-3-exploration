40f62e3bc3188042cfb661960a76a94cf106eb40
xXValhallaCoderXx <renate.gouveia@gmail.com>
2021-09-08 01:42:47 +0800

    Working on game over mechanic

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ gameScene.init = function () {
     health: 100,
     fun: 20,
   };
+
+  this.decayRate = {
+    health: -5,
+    fun: 2,
+  };
 };
 
 // load asset files for our game
@@ -34,8 +39,17 @@ gameScene.create = function () {
   this.bg.on("pointerdown", this.placeItem, this);
 
   this.pet = this.add.sprite(100, 200, "pet").setInteractive();
-
+  // this.pet.depth = 1;
   this.input.setDraggable(this.pet);
+
+  // Eat animation
+  this.anims.create({
+    key: "eating",
+    frames: this.anims.generateFrameNames("pet", { frames: [1, 2, 3] }), //yoyo
+    frameRate: 7,
+    yoyo: true,
+    repeat: 0, // -1 forever
+  });
 
   this.input.on("drag", function (pointer, gameObject, dragX, dragY) {
     gameObject.x = dragX;
@@ -43,6 +57,17 @@ gameScene.create = function () {
   });
 
   this.createUI();
+  this.refreshHud();
+
+  // Decay Health / Fun
+  this.timedEventStats = this.time.addEvent({
+    delay: 1000,
+    repeat: -1,
+    callbackScope: this,
+    callback: function () {
+      this.updateStats(this.decayRate);
+    },
+  });
 };
 
 gameScene.createUI = function () {
@@ -64,7 +89,11 @@ gameScene.createUI = function () {
     fun: 10,
   };
   this.toyBtn.on("pointerdown", this.pickItem);
+
   this.rotateBtn = this.add.sprite(288, 570, "rotate").setInteractive();
+  this.rotateBtn.customStats = {
+    fun: 30,
+  };
   this.rotateBtn.on("pointerdown", this.rotatePet);
 
   this.buttons = [this.appleBtn, this.candyBtn, this.toyBtn, this.rotateBtn];
@@ -73,6 +102,10 @@ gameScene.createUI = function () {
   this.uiBlocked = false;
 
   this.uiReady();
+
+  // Display stats
+  this.createHUD();
+  this.refreshHud();
 };
 
 gameScene.rotatePet = function () {
@@ -83,10 +116,21 @@ gameScene.rotatePet = function () {
   // Block UI during animation
   this.scene.uiBlocked = true;
   this.alpha = 0.5;
-  let scene = this.scene;
-  setTimeout(function () {
-    scene.uiReady();
-  }, 1500);
+
+  // Rotation tween
+  let rotateTween = this.scene.tweens.add({
+    targets: this.scene.pet,
+    duration: 600,
+    angle: 360,
+    pause: false, // So it starts straight away
+    callbackScope: this, // Providde context of scene to "this" in onComplete
+    onComplete: function (tween, sprites) {
+      // increase fun
+      this.scene.updateStats(this.customStats);
+      //set ui to ready
+      this.scene.uiReady();
+    },
+  });
 };
 
 gameScene.pickItem = function () {
@@ -116,21 +160,88 @@ gameScene.placeItem = function (pointer, localX, localY) {
   // Check item selected
 
   if (!this.selectedItem) return;
+  // Check UI
+  if (this.uiBlocked) return;
 
   // Create new item in postion
   let newItem = this.add.sprite(localX, localY, this.selectedItem.texture.key);
 
+  this.uiBlocked = true;
+
+  // Pet Movement
+
+  let petTween = this.tweens.add({
+    targets: this.pet,
+    duration: 500,
+    x: newItem.x,
+    y: newItem.y,
+    paused: false,
+    callbackScope: this,
+    onComplete: function (tween, sprites) {
+      // destroy item
+      newItem.destroy();
+
+      // Event listener for when spritesheeet anim ends
+      this.pet.on(
+        "animationcomplete",
+        function () {
+          // Set pet back to normal frame
+          this.pet.setFrame(0);
+          // Clear UI
+          this.uiReady();
+        },
+        this
+      );
+
+      // Play sprite sheet
+      this.pet.play("eating");
+
+      this.updateStats(this.selectedItem.customStats);
+    },
+  });
+};
+
+gameScene.createHUD = function () {
+  // Health Stat
+  this.healthText = this.add.text(20, 20, "Health: ", {
+    font: "20px Arial",
+    fill: "#fff",
+  });
+
+  this.funText = this.add.text(20, 50, "Fun: ", {
+    font: "20px Arial",
+    fill: "#fff",
+  });
+};
+
+gameScene.refreshHud = function () {
+  this.healthText.setText(`Health: ${this.stats.health}`);
+  this.funText.setText(`Fun: ${this.stats.fun}`);
+};
+
+gameScene.updateStats = function (stats) {
+  let isGameOver = false;
   // this.stats.health += this.selectedItem.customStats.health;
   // this.stats.fun += this.selectedItem.customStats.fun;
-
-  for (stat in this.selectedItem.customStats) {
-    if (this.selectedItem.customStats.hasOwnProperty(stat)) {
-      this.stats[stat] += this.selectedItem.customStats[stat];
+  for (stat in stats) {
+    if (stats.hasOwnProperty(stat)) {
+      this.stats[stat] += stats[stat];
+      if (this.stats[stat] < 0) {
+        isGameOver = true;
+        this.stats[stat] = 0;
+      }
     }
   }
-  console.log("THIS STATS: ", this.stats);
-  // Clear UI
-  this.uiReady();
+
+  this.refreshHud();
+
+  if (isGameOver) {
+    this.gameOver();
+  }
+};
+
+gameScene.gameOver = function () {
+  console.log("GAME OVER");
 };
 
 // our game's configuration
